54056096a2bb5d368cc5ab9162310980a8117c52
thruthesky <thruthesky@gmail.com>
2016-01-13 00:57:18 +0900

    - Adding unit test code - Adding database on codeigniter3

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,11 +1,19 @@
 <h1>This is codeigniter.</h1>
 
-I will plug sapcms4 in.
+<hr>
 
 <?php
 
 
 // Testing a test script.
 use of\database\DatabaseTest;
+use of\entity\EntityTest;
+
 $test = new DatabaseTest();
 $test->run();
+
+
+$entity_test = new EntityTest();
+$entity_test->run();
+
+
